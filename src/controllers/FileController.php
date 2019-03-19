5ef0f86d6632d37e501cc2bfcde1bc63c99ef125
Indra Hermawan <st4rgaze@gmail.com>
2019-03-20 02:36:31 +0700

    image cache custom

--- src/controllers/FileController.php
+++ src/controllers/FileController.php
@@ -1,4 +1,6 @@
-<?php namespace crocodicstudio\crudbooster\controllers;
+<?php
+
+namespace crocodicstudio\crudbooster\controllers;
 
 use File;
 use Image;
@@ -10,41 +12,38 @@ class FileController extends Controller
 {
     public function getPreview($one, $two = null, $three = null, $four = null, $five = null)
     {
-
         if ($two) {
-            $fullFilePath = 'uploads'.DIRECTORY_SEPARATOR.$one.DIRECTORY_SEPARATOR.$two;
-            $filename = $two;
+            $fullFilePath = 'uploads' . DIRECTORY_SEPARATOR . $one . DIRECTORY_SEPARATOR . $two;
+            $filename     = $two;
             if ($three) {
-                $fullFilePath = 'uploads'.DIRECTORY_SEPARATOR.$one.DIRECTORY_SEPARATOR.$two.DIRECTORY_SEPARATOR.$three;
-                $filename = $three;
+                $fullFilePath = 'uploads' . DIRECTORY_SEPARATOR . $one . DIRECTORY_SEPARATOR . $two . DIRECTORY_SEPARATOR . $three;
+                $filename     = $three;
                 if ($four) {
-                    $fullFilePath = 'uploads'.DIRECTORY_SEPARATOR.$one.DIRECTORY_SEPARATOR.$two.DIRECTORY_SEPARATOR.$three.DIRECTORY_SEPARATOR.$four;
-                    $filename = $four;
+                    $fullFilePath = 'uploads' . DIRECTORY_SEPARATOR . $one . DIRECTORY_SEPARATOR . $two . DIRECTORY_SEPARATOR . $three . DIRECTORY_SEPARATOR . $four;
+                    $filename     = $four;
                     if ($five) {
-                        $fullFilePath = 'uploads'.DIRECTORY_SEPARATOR.$one.DIRECTORY_SEPARATOR.$two.DIRECTORY_SEPARATOR.$three.DIRECTORY_SEPARATOR.$four.DIRECTORY_SEPARATOR.$five;
-                        $filename = $five;
+                        $fullFilePath = 'uploads' . DIRECTORY_SEPARATOR . $one . DIRECTORY_SEPARATOR . $two . DIRECTORY_SEPARATOR . $three . DIRECTORY_SEPARATOR . $four . DIRECTORY_SEPARATOR . $five;
+                        $filename     = $five;
                     }
                 }
             }
         } else {
-            $fullFilePath = 'uploads'.DIRECTORY_SEPARATOR.$one;
-            $filename = $one;
+            $fullFilePath = 'uploads' . DIRECTORY_SEPARATOR . $one;
+            $filename     = $one;
         }
 
-        $fullStoragePath = storage_path('app/'.$fullFilePath);
-        $lifetime = 31556926; // One year in seconds
+        $fullStoragePath = storage_path('app/' . $fullFilePath);
+        $lifetime        = 31556926; // One year in seconds
 
-        
+        $handler = new \Symfony\Component\HttpFoundation\File\File(storage_path('app/' . $fullFilePath));
 
-        if (! Storage::exists($fullFilePath)) {
+        if (!Storage::exists($fullFilePath)) {
             abort(404);
         }
 
-        $handler = new \Symfony\Component\HttpFoundation\File\File(storage_path('app/'.$fullFilePath));
-        
-        $extension = strtolower(File::extension($fullStoragePath));
-        $images_ext = config('crudbooster.IMAGE_EXTENSIONS', 'jpg,png,gif,bmp');
-        $images_ext = explode(',', $images_ext);
+        $extension     = strtolower(File::extension($fullStoragePath));
+        $images_ext    = config('crudbooster.IMAGE_EXTENSIONS', 'jpg,png,gif,bmp');
+        $images_ext    = explode(',', $images_ext);
         $imageFileSize = 0;
 
         if (in_array($extension, $images_ext)) {
@@ -54,16 +53,19 @@ class FileController extends Controller
                 $h = Request::get('h') ?: $w;
             } else {
                 $w = Request::get('w');
-                $h = Request::get('h') ?: $w;
+                $h = Request::get('h');
+                $t = Request::get('t');
             }
 
             $imgRaw = Image::cache(function ($image) use ($fullStoragePath, $w, $h) {
                 $im = $image->make($fullStoragePath);
                 if ($w) {
-                    if (! $h) {
-                        $im->fit($w);
+                    if (!$h) {
+                        $im->resize($w, null, function ($constraint) {
+                            $constraint->aspectRatio();
+                        });
                     } else {
-                        $im->fit($w, $h);
+                        $im->resize($w, $h);
                     }
                 }
 
@@ -78,19 +80,19 @@ class FileController extends Controller
          */
         $file_time = $handler->getMTime(); // Get the last modified time for the file (Unix timestamp)
 
-        $header_content_type = $handler->getMimeType();
+        $header_content_type   = $handler->getMimeType();
         $header_content_length = ($imageFileSize) ?: $handler->getSize();
-        $header_etag = md5($file_time.$fullFilePath);
-        $header_last_modified = gmdate('r', $file_time);
-        $header_expires = gmdate('r', $file_time + $lifetime);
+        $header_etag           = md5($file_time . $fullFilePath);
+        $header_last_modified  = gmdate('r', $file_time);
+        $header_expires        = gmdate('r', $file_time + $lifetime);
 
         $headers = [
-            'Content-Disposition' => 'inline; filename="'.$filename.'"',
-            'Last-Modified' => $header_last_modified,
-            'Cache-Control' => 'must-revalidate',
-            'Expires' => $header_expires,
-            'Pragma' => 'public',
-            'Etag' => $header_etag,
+            'Content-Disposition' => 'inline; filename="' . $filename . '"',
+            'Last-Modified'       => $header_last_modified,
+            'Cache-Control'       => 'must-revalidate',
+            'Expires'             => $header_expires,
+            'Pragma'              => 'public',
+            'Etag'                => $header_etag,
         ];
 
         /**
@@ -100,7 +102,7 @@ class FileController extends Controller
         $h2 = isset($_SERVER['HTTP_IF_NONE_MATCH']) && str_replace('"', '', stripslashes($_SERVER['HTTP_IF_NONE_MATCH'])) == $header_etag;
 
         $headers = array_merge($headers, [
-            'Content-Type' => $header_content_type,
+            'Content-Type'   => $header_content_type,
             'Content-Length' => $header_content_length,
         ]);
 
@@ -112,9 +114,9 @@ class FileController extends Controller
             }
         } else {
             if (Request::get('download')) {
-                return Response::download(storage_path('app/'.$fullFilePath), $filename, $headers);
+                return Response::download(storage_path('app/' . $fullFilePath), $filename, $headers);
             } else {
-                return Response::file(storage_path('app/'.$fullFilePath), $headers);
+                return Response::file(storage_path('app/' . $fullFilePath), $headers);
             }
         }
     }
